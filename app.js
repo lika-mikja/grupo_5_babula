89c4225db064a5e14849079ca6812571efa3fcbc
Anjelika Mikhailovna <anjelikamikhailovna@MacBook-Pro-de-Anjelika.local>
2022-05-04 19:07:09 -0300

    Nuevos cambios

--- app.js
+++ app.js
@@ -18,22 +18,22 @@ app.listen(4000, () => {
 
 // Rutas 
 app.get('/login', (req, res) => { 
-    res.sendFile(path.resolve(__dirname , './views/login.html'))
+    res.sendFile(path.resolve(__dirname , './views/login.ejs'))
 });
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
+    res.sendFile(path.resolve(__dirname, './views/register.ejs'))
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+    res.sendFile(path.resolve(__dirname, "./views/index.ejs"))
 });
 
 app.get('/shop', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/shop.html"))
+    res.sendFile(path.resolve(__dirname, "./views/shop.ejs"))
 });
 
 app.get('/product', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/product.html"))
+    res.sendFile(path.resolve(__dirname, "./views/product.ejs"))
 });
 

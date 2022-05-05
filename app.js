a5cc928cf8112ea743eea472e228c5e475b496a1
Anjelika Mikhailovna <anjelikamikhailovna@MacBook-Pro-de-Anjelika.local>
2022-05-05 19:00:50 -0300

    check nuevos cambios

--- app.js
+++ app.js
@@ -10,31 +10,31 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 
 // Configuración
 app.set ("view engine", "ejs");
-app.set("views", "./views/pages"); /* Indica a express donde buscar las vistas */
+app.set("views", path.join(__dirname, "./src/views")); /* Indica a express donde buscar las vistas */
 
+
+
+// Rutas 
+app.get('/login', (req, res) => { 
+    res.sendFile(path.resolve(__dirname , './src/views/users/login'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/users/register'))
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./src/views/users/index"))
+ });
+
+app.get('/shop', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./src/views/products/shop"))
+});
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./src/views/products/product"))
+});
 
 app.listen(4000, () => {
     console.log('Servidor funcionando en el puerto http://localhost:4000');
 });
-
-// Rutas 
-app.get('/login', (req, res) => { 
-    res.sendFile(path.resolve(__dirname , './views/login.ejs'))
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.ejs'))
-});
-
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.ejs"))
-});
-
-app.get('/shop', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/shop.ejs"))
-});
-
-app.get('/product', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/product.ejs"))
-});
-
